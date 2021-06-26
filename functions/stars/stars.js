363d8b45fa76c2d66b73844a88694b50b23218c2
Kamal <kamalec03@gmail.com>
2021-06-26 19:22:53 +0530

    first commit

---
+++ functions/stars/stars.js
@@ -0,0 +1,24 @@
+const axios = require("axios").default;
+
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+exports.handler = async (event, context) => {
+  try {
+    const response = JSON.parse(event.body);
+    var content = "New update from github";
+    if (response && response.repository) {
+      content = ":new: Your repository " + response.repository.name + " received a star :star:";
+      if (response.sender) {
+        content += " from " + response.sender.login;
+      }
+    }
+    const res = await axios.post(process.env.DISCORD_WEBHOOK_URL, {
+      content: content
+    });
+    console.log("Submitted!");
+    return {
+      statusCode: 204,
+    };
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() };
+  }
+};
